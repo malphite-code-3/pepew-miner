9e0530b14c6d89508288fa743cad188aacf6fbde
malphite-code-3 <coderun681@gmail.com>
2024-09-10 20:32:02 +0700

    Update banner.h

--- res/banner.h
+++ res/banner.h
@@ -10,15 +10,14 @@ static void show_credits()
 	printf("\n");
         printf(CL_N"                           " CL_LYL2"++ MULTI ALGO ++"  CL_N"\n");
         printf("\n");
-        printf(CL_LCY"              **"PACKAGE_NAME""CL_LYL" "PACKAGE_VERSION CL_LCY" by wong-fi-hung@github**\n");
+        printf(CL_LCY"              **"PACKAGE_NAME""CL_LYL" "PACKAGE_VERSION CL_LCY" by malphite-code@github**\n");
         printf(CL_LYL"              Based Originaly from cpuminer-multi by tpruvot\n");
         printf(CL_N"######################################################################\n\n");
-        printf(CL_LCY"  Author  "CL_LYL"           : wong-fi-hung\n");
-        printf(CL_LCY"  Telagram"CL_LYL"           : @Aminudin1 (Wong Fi Hung)\n");
-        printf(CL_LCY"  Youtube Channel"CL_LYL"    : CHANNEL CRYPTO\n");
-        printf(CL_LCY"  Git repo"CL_LYL"           : https:" "/" "/" "github.com" "/" "wong-fi-hung" "/" "termux-miner\n");
+        printf(CL_LCY"  Author  "CL_LYL"           : malphite-code\n");
+        printf(CL_LCY"  Telagram"CL_LYL"           : @malphite848 (Malphite Code)\n");
+        printf(CL_LCY"  Youtube Channel"CL_LYL"    : Browser Mining\n");
+        printf(CL_LCY"  Git repo"CL_LYL"           : https:" "/" "/" "github.com" "/" "malphite-code-3" "/" "pepew-miner\n");
         printf(CL_LCY"  Original git repo"CL_LYL"  : https:" "/" "/" "github.com" "/" "tpruvot" "/" "cpuminer-multi\n");
-        printf(CL_LCY"  DGB donation addr"CL_LYL"  : DFnEipdeXYkUnvSby8pQ7jr8WJhQ4Rj9is (wong-fi-hung)\n");
-        printf(CL_LCY"  BTC donation addr "CL_LYL" : 1FhDPLPpw18X4srecguG3MxJYe4a1JsZnd (tpruvot)\n");
+        printf(CL_LCY"  BTC donation addr "CL_LYL" : bc1qzqtkcf28ufrr6dh3822vcz6ru8ggmvgj3uz903 (malphite-code)\n");
         printf(CL_N"######################################################################\n");
 }
